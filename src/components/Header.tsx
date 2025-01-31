92864795978a80d0dcec81b435592f494057ad56
Rozioi <gtavvedeos@gmail.com>
2025-01-31 21:05:18 +0300

    Update website design and select a new layout; started styling the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,36 +1,29 @@
 import React from 'react';
 import { NavLink } from "react-router-dom";
 import styles from '../assets/Header.module.scss';
-import { FaRegUser } from "react-icons/fa";
 
 const Header: React.FC = () => {
     return (
         <div className={styles.header}>
-            <div className={styles.navContainer}>
-                <div>
-                    <img src='/logo.png' alt='logo' className={styles.logo} />
-                </div>
                 <div className={styles.navLinkList}>
                     <NavLink to={'/'} className={({ isActive }) => isActive ? `${styles.link} ${styles.activeLink}` : styles.link}>
-                        Home
-                    </NavLink>
-                    <NavLink to={'/4'} className={({ isActive }) => isActive ? `${styles.link} ${styles.activeLink}` : styles.link}>
-                        Product
+                        Marketplace
                     </NavLink>
                     <NavLink to={'/1'} className={({ isActive }) => isActive ? `${styles.link} ${styles.activeLink}` : styles.link}>
-                        Pricing
+                        Artists
                     </NavLink>
                     <NavLink to={'/2'} className={({ isActive }) => isActive ? `${styles.link} ${styles.activeLink}` : styles.link}>
-                        About
+                        Community
                     </NavLink>
                     <NavLink to={'/3'} className={({ isActive }) => isActive ? `${styles.link} ${styles.activeLink}` : styles.link}>
-                        Contact
+                        Collections
                     </NavLink>
+
                 </div>
-                <div className={styles.subscribeContainer}>
-                    <input type='text' placeholder='Your email' className={styles.subscribeInput} />
-                    <button className={styles.subscribeButton}>Subscribe</button>
-                </div>
+            <div>
+                <button className={styles['btn-contact']}>
+                    Contact
+                </button>
             </div>
         </div>
     );
